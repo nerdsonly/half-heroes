aa315083182f54793da91e29d34eca672eeea181
nerdsonly <o.onerdsonly@gmail.com>
2019-02-09 18:14:27 -0200

    Uptei Plugins+Correção de Bugs
    Por gentileza...Testem

--- js/plugins.js
+++ js/plugins.js
@@ -3,5 +3,20 @@
 var $plugins =
 [
 {"name":"ParallaxLayerMap","status":true,"description":"多層レイヤー一枚絵マッププラグイン","parameters":{}},
-{"name":"Foreground","status":true,"description":"マップに合わせてスクロールする近景の設定(Ver1.0.1)","parameters":{}}
+{"name":"Foreground","status":true,"description":"マップに合わせてスクロールする近景の設定(Ver1.0.1)","parameters":{}},
+{"name":"WeaponSkill","status":true,"description":"Change skill id of attack for each weapon.","parameters":{}},
+{"name":"AltMenuScreen","status":true,"description":"Alternative menu screen layout.","parameters":{}},
+{"name":"AltSaveScreen","status":true,"description":"Alternative save/load screen layout.","parameters":{}},
+{"name":"Community_Basic","status":true,"description":"Plugin used to set basic parameters.","parameters":{"cacheLimit":"10","screenWidth":"816","screenHeight":"624","changeWindowWidthTo":"","changeWindowHeightTo":"","renderingMode":"auto","alwaysDash":"off"}},
+{"name":"EnemyBook","status":true,"description":"Displays detailed statuses of enemies.","parameters":{"Unknown Data":"??????"}},
+{"name":"Foreground","status":true,"description":"display foreground tiling sprite(Ver1.0.1)","parameters":{}},
+{"name":"ItemBook","status":true,"description":"Displays detailed statuses of items.","parameters":{"Unknown Data":"??????","Price Text":"Price","Equip Text":"Equip","Type Text":"Type"}},
+{"name":"MadeWithMv","status":true,"description":"Show a Splash Screen \"Made with MV\" and/or a Custom Splash Screen before going to main screen.","parameters":{"Show Made With MV":"true","Made with MV Image":"MadeWithMv","Show Custom Splash":"false","Custom Image":"","Fade Out Time":"120","Fade In Time":"120","Wait Time":"160"}},
+{"name":"ParallaxLayerMap","status":true,"description":"ParallaxLayerMapPlugin","parameters":{}},
+{"name":"SAN_ShaderFilter","status":true,"description":"Shader filter 1.0.0\r\nVisual effect by shader filter","parameters":{}},
+{"name":"SAN_ShaderFilterCharaHeatHaze","status":true,"description":"Character heat haze effect filter 1.0.0\r\nEffect filter which displays the effect of the heat haze around the character.","parameters":{}},
+{"name":"SAN_ShaderFilterCharaRipple","status":true,"description":"Character ripple effect filter 1.0.0\r\nDisplay a ripple effect around the character.","parameters":{}},
+{"name":"SAN_ShaderFilterScanLine","status":true,"description":"Scan line effect filter 1.0.0\r\nDisplay scan lines on the sprite.","parameters":{}},
+{"name":"TitleCommandPosition","status":true,"description":"Changes the position of the title command window.","parameters":{"Offset X":"0","Offset Y":"0","Width":"240","Background":"0"}},
+{"name":"YEP_SaveEventLocations","status":true,"description":"v1.03 Habilita mapas específicos a memorizarem as\nlocalizações de eventos quando saindo e carregando-os após re-entrar no mapa.","parameters":{}}
 ];
